0ba76c4b9d4e046b2d8ffe4dff55b2e1d15344cb
msakib10 <129744604+msakib10@users.noreply.github.com>
2026-04-02 15:54:36 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages-এ লাইভ করার জন্য নিচের base লাইনটি যোগ করা হয়েছে
+  base: '/Sakib-Store-New/', 
 })
